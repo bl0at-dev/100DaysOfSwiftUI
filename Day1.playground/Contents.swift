a8e18ea9fe27306c1483ba8a70081c4e92103e50
bl0at <36534822+bl0at-dev@users.noreply.github.com>
2026-05-20 19:52:01 +0200

    Initial Commit

---
+++ Day1.playground/Contents.swift
@@ -0,0 +1,58 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+//
+//let character = "Ted"
+//
+//var playerName = "Roy"
+//print(playerName)
+//
+//let actor = "Denzel Washington"
+//
+//let quote = "Then he tapped a sigh saying \"Believe\" and walked away."
+//print(quote)
+//
+//let anotherQuote = """
+//Kiedyś
+//to
+//było
+//"""
+//print(anotherQuote)
+//
+//print(actor.isEmpty)
+//
+//let nameLength = playerName.count
+//print(nameLength)
+//
+//print(character.uppercased())
+//
+//let movie = """
+//A day in
+//the life of an
+//Apple engineer
+//"""
+//
+//print(movie.hasPrefix("A day"))
+//print(anotherQuote.hasSuffix("było"))
+//let score = 10_0
+//
+//let lowerScore = score - 3
+//
+//print(score)
+//print(lowerScore)
+//
+//var counter = 20
+//counter -= 2
+//print(counter)
+//
+//let number = 50
+//print(number.isMultiple(of: 5))
+let number = 0.1 + 0.2
+print(number)
+
+let a = 1
+let b = 4.0
+let c = a + Int(b)
+print(c)
+print(c)
+
---
+++ Day2.playground/Contents.swift
@@ -0,0 +1,41 @@
+import Cocoa
+
+var greeting = "Hello, playground"
+
+//let goodDogs = true
+//
+//var gameOver = false
+//print(gameOver)
+//gameOver.toggle()
+//print(gameOver)
+
+
+//let isMultiple = 120.isMultiple(of: 4)
+//
+//var isAuthenticated = false
+//isAuthenticated = !isAuthenticated
+//print(isAuthenticated)
+//isAuthenticated = !isAuthenticated
+//print(isAuthenticated)
+
+//let name = "Karol"
+//let age = 26
+//let message = "Hello, my name is \(name) and I'm \(age) years old."
+//print(message)
+//
+//print("5 x 5 is \(5 * 5)")
+
+// Checkpoint 1
+
+let temperatureInCelsius = 25.0
+let temperatureInFahrenheit = ((temperatureInCelsius * 9) / 5) + 32
+let result = "Temperature in Celsius is \(temperatureInCelsius)°C and in Fahrenheit is \(temperatureInFahrenheit)°F"
+print(result)
+
+
+
+
+
+
+
+
