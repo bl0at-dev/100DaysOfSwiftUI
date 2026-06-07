e9b9af16d6d19d82b240b3e872b4747e80f0e09c
bl0at <36534822+bl0at-dev@users.noreply.github.com>
2026-06-07 13:59:21 +0200

    Completed the overview for the WeSplit project!

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Karol Kusowski on 07/06/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Gooood morning Vietnam!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Karol Kusowski on 07/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
