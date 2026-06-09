b167c9d622e75322e94109fffa28a0ecb0153d3b
bl0at <36534822+bl0at-dev@users.noreply.github.com>
2026-06-09 19:34:49 +0200

    Finished day 18!

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -10,26 +10,24 @@ import SwiftUI
 struct ContentView: View {
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
-    @State private var tipPercentage = 20
+    @State private var tipPercentage = 0
     @FocusState private var amountIsFocused: Bool
-    
-    let tipPercentages = [10, 15, 20, 25, 0]
+    let localCurrency = Locale.current.currency?.identifier ?? ""
+        
+    var grandTotal: Double {
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount / 100 * tipSelection
+        return checkAmount + tipValue
+    }
     
     var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
-        let tipSelection = Double(tipPercentage)
-
-        let tipValue = checkAmount / 100 * tipSelection
-        let grandTotal = checkAmount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
-
-        return amountPerPerson
+        grandTotal / Double(numberOfPeople + 2)
     }
     var body: some View {
         NavigationStack {
             Form {
                 Section("Type the bill amount") {
-                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? ""))
+                    TextField("Amount", value: $checkAmount, format: .currency(code: localCurrency))
                         .keyboardType(.decimalPad)
                         .focused($amountIsFocused)
                     
@@ -43,15 +41,19 @@ struct ContentView: View {
                 
                 Section("How much do you want to tip?") {
                     Picker("Tip percentage", selection: $tipPercentage) {
-                        ForEach(tipPercentages.sorted(), id: \.self) {
+                        ForEach(1..<101) {
                             Text($0, format: .percent)
                         }
                     }
                 }
-                .pickerStyle(.segmented)
+                .pickerStyle(.navigationLink)
                 
-                Section("Total per person") {
-                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? ""))
+                Section("Total amount with tip") {
+                    Text(grandTotal, format: .currency(code: localCurrency))
+                }
+                
+                Section("Amount per person") {
+                    Text(totalPerPerson, format: .currency(code: localCurrency))
                 }
             }
             .navigationTitle("WeSplit")
