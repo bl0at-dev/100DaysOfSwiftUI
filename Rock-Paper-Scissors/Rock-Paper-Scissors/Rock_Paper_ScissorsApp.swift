9e9bc7e8228200814aaa24f613794553ed376887
bl0at <36534822+bl0at-dev@users.noreply.github.com>
2026-06-18 05:31:36 +0200

    Finished day 25!

---
+++ Rock-Paper-Scissors/Rock-Paper-Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock-Paper-Scissors
+//
+//  Created by Karol Kusowski on 17/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
