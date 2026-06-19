0da69804a23a174ae08ca0ea73a441338d9116de
bl0at <36534822+bl0at-dev@users.noreply.github.com>
2026-06-19 06:47:36 +0200

    Finished day 26 and 27!

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Karol Kusowski on 18/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
