9b457dc44603a64f2cc9b17864463fa1d01bdbed
bl0at <36534822+bl0at-dev@users.noreply.github.com>
2026-06-21 06:44:08 +0200

    Finished day 29!

---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Karol Kusowski on 21/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
